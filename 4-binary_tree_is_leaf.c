14a502a096321c92576f63d2c0e2505da628a596
Beka Ensarmu <beekanensarmu@gmail.com>
2023-11-01 23:00:09 -0700

    binary_tree_is_leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,12 +8,6 @@
  *         Otherwise - 0.
  */
 
- typedef struct binary_tree_s {
-    int n;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-} binary_tree_t;
-
 int binary_tree_is_leaf(const binary_tree_t *node) {
     if (node == NULL) {
         return 0;
