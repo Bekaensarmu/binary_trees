d65ef703f591a70169638a3a6d6a7140a44e60f7
Beka Ensarmu <beekanensarmu@gmail.com>
2023-11-02 00:01:54 -0700

    binary_tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,11 +10,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t n = 0, m = 0;
+		size_t l = 0, r = 0;
 
-		n = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		m = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((n > r) ? l : m);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
 	}
 	return (0);
 }
