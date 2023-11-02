ff74be6f10d5cf4db946df3cdfd0711b1bbac8d5
Beka Ensarmu <beekanensarmu@gmail.com>
2023-11-02 16:19:39 -0700

    16-binary_tree_is_perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: If tree is NULL or not perfect, 0.
+ *         Otherwise, 1.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (is_perfect_recursive(tree, depth(get_leaf(tree)), 0));
+}
